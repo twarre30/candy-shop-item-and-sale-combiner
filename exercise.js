cfe8cf74c854917bfc1152144c5c815f07210533
Kyle Coberly <kyle.coberly@gmail.com>
2022-03-21 12:53:17 -0600

    Remove solution

--- exercise.js
+++ exercise.js
@@ -1,23 +1,8 @@
-function getItemById(items, id){
-    return items.filter(item => item.id == id)[0];
-}
-
-function buildTransactions(sales, items){
-    return sales.map(sale => {
-        return Object.assign(sale, getItemById(items, sale.itemId));
-    }).map(sale => {
-        delete sale.id;
-        return sale;
-    });
-}
-
-function getTransactionsByItemDescription(transactions, description){
-    return transactions.filter(transaction => transaction.description === description);
-}
+// Your code goes here
 
 module.exports = {
     // Uncomment these as you write them
-    getItemById,
-    buildTransactions,
-    getTransactionsByItemDescription
+    // getItemById,
+    // buildTransactions,
+    // getTransactionsByItemDescription
 }
